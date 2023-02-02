45a8892a675f130418cede2b06c26a47f95d233d
Igor Krupenja <igor.krupenja@gmail.com>
2023-02-02 19:54:34 +0200

    WIP adding vitest

--- apps/web/__tests__/index.test.tsx
+++ apps/web/__tests__/index.test.tsx
@@ -1,15 +1,28 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
 
-import Home from '@/pages/index';
+// import Home from '../pages/index';
+// // todo
+// // import Home from '@/pages/index';
 
-describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />);
+// describe('Home', () => {
+//   it('renders a heading', () => {
+//     render(<Home />);
 
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
-    });
+//     const heading = screen.getByRole('heading', {
+//       name: /welcome to next\.js!/i,
+//     });
 
-    expect(heading).toBeInTheDocument();
-  });
+//     expect(heading).toBeInTheDocument();
+//   });
+// });
+
+import { render, screen, within } from '@testing-library/react';
+import { expect, test } from 'vitest';
+
+import Home from '../pages';
+
+test('home', () => {
+  render(<Home />);
+  const main = within(screen.getByRole('main'));
+  expect(main.getByRole('heading', { level: 1, name: /welcome to next\.js!/i })).toBeDefined();
 });
