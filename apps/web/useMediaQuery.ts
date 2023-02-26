f187f90b8d69405cbebb7ebbea6768804e235571
Igor Krupenja <igor.krupenja@gmail.com>
2023-02-26 20:23:58 +0200

    Clean up

--- apps/web/useMediaQuery.ts
+++ apps/web/useMediaQuery.ts
@@ -4,16 +4,14 @@ export function useMediaQuery(isMobileUserAgent: boolean): boolean {
   const [isMobile, setIsMobile] = useState(isMobileUserAgent);
 
   useEffect(() => {
+    const changeSize = (event: MediaQueryListEvent) => setIsMobile(event.matches);
+
     const mediaQueryList = window.matchMedia('(max-width: 1044px)');
+    mediaQueryList.addEventListener('change', changeSize);
 
-    const handleMediaQueryChange = (event: MediaQueryListEvent) => {
-      setIsMobile(event.matches);
-    };
-
-    mediaQueryList.addEventListener('change', handleMediaQueryChange);
     setIsMobile(mediaQueryList.matches);
 
-    return () => mediaQueryList.removeEventListener('change', handleMediaQueryChange);
+    return () => mediaQueryList.removeEventListener('change', changeSize);
   }, []);
 
   return isMobile;
