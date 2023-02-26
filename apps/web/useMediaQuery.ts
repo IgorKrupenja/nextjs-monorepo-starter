3e0d2961db9412c5b3de07d9c7dd5d3538d78371
Igor Krupenja <igor.krupenja@gmail.com>
2023-02-26 20:32:08 +0200

    Add comments

--- apps/web/useMediaQuery.ts
+++ apps/web/useMediaQuery.ts
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 
 export function useMediaQuery(isMobileUserAgent: boolean): boolean {
+  // On the server,l use the user agent to determine if the user is on a mobile device
   const [isMobile, setIsMobile] = useState(isMobileUserAgent);
 
   useEffect(() => {
+    // On the client, use the media query API to determine if the user is on a mobile device
     const changeSize = (event: MediaQueryListEvent) => setIsMobile(event.matches);
 
     const mediaQueryList = window.matchMedia('(max-width: 1044px)');
