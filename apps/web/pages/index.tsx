406689a0840d11f4b67b4e1de69e074b1a917f32
Igor Krupenja <igor.krupenja@gmail.com>
2023-02-26 19:02:37 +0200

    Add test button with state

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,12 @@
 import Head from 'next/head';
+import { useState } from 'react';
 import { Button } from 'ui';
 
 import styles from './index.module.scss';
 
 export default function Home() {
+  const [num, setNum] = useState(13);
+
   return (
     <>
       <Head>
@@ -19,6 +22,11 @@ export default function Home() {
         <p className={styles.description}>
           Get started by editing <code>pages/index.tsx</code>
         </p>
+
+        <div>
+          <button onClick={() => setNum(num + 1)}>CLICK</button>
+          <h2>Number: {num}</h2>
+        </div>
 
         <Button />
       </main>
