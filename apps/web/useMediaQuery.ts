b0a53f1bfbc297742768486e27222040b27cd321
Igor Krupenja <igor.krupenja@gmail.com>
2023-02-26 20:36:28 +0200

    Change media query

--- apps/web/useMediaQuery.ts
+++ apps/web/useMediaQuery.ts
@@ -8,7 +8,7 @@ export function useMediaQuery(isMobileUserAgent: boolean): boolean {
     // On the client, use the media query API to determine if the user is on a mobile device
     const changeSize = (event: MediaQueryListEvent) => setIsMobile(event.matches);
 
-    const mediaQueryList = window.matchMedia('(max-width: 1044px)');
+    const mediaQueryList = window.matchMedia('(max-width: 1440px)');
     mediaQueryList.addEventListener('change', changeSize);
 
     setIsMobile(mediaQueryList.matches);
