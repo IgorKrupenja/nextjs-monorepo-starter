33e838c54941bb2db1328d3b3d2f44e75dfe56ab
Igor Krupenja <igor.krupenja@gmail.com>
2023-07-22 11:44:29 +0300

    Remove button styles

--- packages/shared/components/Button/Button.tsx
+++ packages/shared/components/Button/Button.tsx
@@ -1,5 +1,3 @@
-import styles from './Button.module.scss';
-
 export const Button = (): JSX.Element => {
-  return <button className={styles.button}>Boom!</button>;
+  return <button>Boom!</button>;
 };
